9d557c33fe86977c8764da02e63bd8afe468b6a4
Madmaxvas <maxmadvas@gmail.com>
2025-11-24 12:57:34 +0300

    Planar draw

--- main.cpp
+++ main.cpp
@@ -1,4 +1,34 @@
+#include <iostream>
+
+struct Planar {
+  virtual int x()const;
+  virtual int y()const;
+  virtual int abs_sqr()const;
+  virtual ~Planar() = default;
+};
+
+Planar * make(std::istream & is);
+Planar * most_left(Planar ** pls, size_t k);
+Planar * make(size_t pl);
+void draw(Planar * pl);
+void free_planars(Planar ** pls, size_t k);
+
 int main()
 {
-return 0;
+  Planar * pls[10] = {};
+  size_t k = 0;
+  for (size_t i = 0; i < 10; ++i){
+    try{
+      pls[k] = make(i%2);
+    }catch(...){
+      free_planars(pls,k);
+      return 2;
+    }
+    k++;
+  }
+  //pls[k++] = make(std::cin);
+  Planar * most_left = most_left(pls,k);
+  draw(most_left(pls,k))
+  free_planars(pls,k);
+  return 0;
 }
