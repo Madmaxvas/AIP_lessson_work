af0599292a37d3fde24804f1377482962550e5e4
Madmaxvas <maxmadvas@gmail.com>
2025-11-24 13:29:48 +0300

    Main.cpp end lesson

--- main.cpp
+++ main.cpp
@@ -1,54 +1,96 @@
 #include <iostream>
+#include <stdexcept>
 
 struct Planar {
-  virtual int x()const;
-  virtual int y()const;
-  virtual int abs_sqr()const;
+  virtual int x() const = 0;
+  virtual int y() const = 0;
+  virtual int abs_sqr() const = 0;
   virtual ~Planar() = default;
 };
 
-Planar * make(std::istream & is);
-Planar * most_left(Planar ** pls, size_t k);
-Planar * make(size_t pl);
-void draw(Planar * pl);
-void free_planars(Planar ** pls, size_t k);
+struct Point : Planar {
+    Point(int xx, int yy);  // Объявление конструктора
+    virtual int x() const;
+    virtual int y() const;
+    virtual int abs_sqr() const;
+    virtual ~Point() = default;
+  private:
+    int data[2];
+};
 
-Planar * make(size_t pl){
-  switch (pl){
-    default;
-      throw std::logic_error("bad id");
-  }
-  return nullptr;
+// Определение конструктора Point
+Point::Point(int xx, int yy) {
+    data[0] = xx;
+    data[1] = yy;
 }
 
-void free_planars(Planar ** pls, size_t k){
-  for(size_t i = 0; i < k;++i){
+int Point::x() const{
+  return data[0];
+}
+
+int Point::y() const{
+  return data[1];
+}
+
+int Point::abs_sqr() const{
+  return x()*x()+y()*y();
+}
+
+Planar* make(std::istream & is);
+Planar* most_left(Planar** pls, size_t k);
+Planar* make(size_t pl);
+void draw(Planar* pl);
+void free_planars(Planar** pls, size_t k);
+
+Planar* make(size_t id){
+  Planar* r = nullptr;  // Исправлено: добавлено =
+  switch(id){
+    case 0:  // Исправлено: : вместо ;
+      r = new Point(0,0);
+      break;
+    default:
+      throw std::logic_error("bad id");
+  }
+  return r;
+}
+
+void free_planars(Planar** pls, size_t k) {
+  for(size_t i = 0; i < k; ++i) {
     delete pls[i];
   }
 }
 
-void draw(Planar * pls){
-  std::cout << pl->x();
-  std::cout << " ";
-  std::cout << pl -> y() << "y";
+void draw(Planar* pl) {
+  std::cout << pl->x() << " " << pl->y();
 }
 
-int main()
-{
-  Planar * pls[10] = {};
+Planar* most_left(Planar** pls, size_t k) {
+  if (k == 0) return nullptr;
+  Planar* leftmost = pls[0];
+  for (size_t i = 1; i < k; ++i) {
+    if (pls[i]->x() < leftmost->x()) {
+      leftmost = pls[i];
+    }
+  }
+  return leftmost;
+}
+
+int main() {
+  Planar* pls[10] = {};
   size_t k = 0;
-  for (size_t i = 0; i < 10; ++i){
-    try{
-      pls[k] = make(i%2);
-    }catch(...){
-      free_planars(pls,k);
+  for (size_t i = 0; i < 10; ++i) {
+    try {
+      pls[k] = make(i % 2);
+      k++;
+    } catch(...) {
+      free_planars(pls, k);
       return 2;
     }
-    k++;
   }
-  //pls[k++] = make(std::cin);
-  Planar * most_left = most_left(pls,k);
-  draw(most_left(pls,k))
-  free_planars(pls,k);
+
+  Planar* leftmost = most_left(pls, k);
+  draw(leftmost);
+  free_planars(pls, k);
   return 0;
 }
+
